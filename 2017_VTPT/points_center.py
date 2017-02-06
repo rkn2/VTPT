875e09f1343ee360d01f85ca0d6f3f965a4b7c04
Becca Napolitano <rkn2@princeton.edu>
2017-02-06 15:36:56 -0500

    Center Points x y x

--- 2017_VTPT/points_center.py
+++ 2017_VTPT/points_center.py
@@ -1,6 +1,22 @@
+import numpy as np
 
 #test set of points
 a = np.array([[-11.54337120056152, -7.660790920257568, 0],
 [-11.54337120056152, -7.660790920257568, 16.0474910736084],
 [3.485972881317139, -7.660790920257568, 0],
 [3.485972881317139, -7.660790920257568, 16.0474910736084]])
+
+#calculate the center
+#find out how many points you have
+numPoints = len(a)
+#calculate mean x
+xPoints = a[:,0]
+avgX = np.average(xPoints)
+#calculate mean y
+yPoints = a[:,1]
+avgY = np.average(yPoints)
+#calculate mean z
+zPoints = a[:,1]
+avgZ = np.average(zPoints)
+
+center = [avgX, avgY, avgZ]
